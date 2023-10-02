aee1e32e9688465a4f5d985b68e753d940c8e76e
Khoa_HD <97817877+khoa36isefiw@users.noreply.github.com>
2023-10-02 23:06:04 +0700

    Delete 20110375 directory

--- 20110375/middlewares/logger.js
+++
@@ -1,9 +0,0 @@
-function requestLogger(req, res, next) {
-    const method = req.method; // Lấy phương thức (POST hoặc GET)
-    const url = req.originalUrl; // Lấy URL của request
-  
-    console.log(`Request Method: ${method}, URL: ${url}`);
-    next(); // Chuyển tiếp yêu cầu cho middleware hoặc route tiếp theo
-}
-  
-module.exports = requestLogger;
