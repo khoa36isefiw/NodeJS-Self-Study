fa2612d559f88c6caf143efdda50713c0a2651f8
khoa36isefiw <huynhdangkhoa961617@gmail.com>
2023-10-29 23:05:42 +0700

    make nasaa lord

---
+++ Nasa_20110375/client/src/hooks/requests.js
@@ -0,0 +1,56 @@
+const API_URL = 'http://localhost:8000'
+
+async function httpGetPlanets() {
+  const res = await fetch(`${API_URL}/planets`);
+  return await res.json();
+}
+
+// Load launches, sort by flight number, and return as JSON.
+async function httpGetLaunches() {
+  const res = await fetch(`${API_URL}/launches`);
+  const fetchLaunches =  await res.json();
+  return fetchLaunches.sort((a,b) => a.flightNumber - b.flightNumber);
+}
+
+// Submit given launch data to launch system.
+async function httpSubmitLaunch(launch) {
+  try {
+    await fetch(`${API_URL}/launches`, {
+      method: "post",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(launch),
+    })
+    return {
+      ok: true
+    }
+  } catch (error) {
+    return {
+      ok: false
+    }
+  }
+}
+
+  // Delete launch with given ID.
+async function httpAbortLaunch(id) {
+  try {
+    await fetch(`${API_URL}/launches/${id}`, {
+      method: "delete",
+    })
+    return {
+      ok: true
+    }
+  } catch (error) {
+    return {
+      ok: false
+    }
+  }
+}
+
+export {
+  httpGetPlanets,
+  httpGetLaunches,
+  httpSubmitLaunch,
+  httpAbortLaunch,
+};
---
+++ Nasa_20110375/server/src/routes/planets/planets.controller.js
@@ -0,0 +1,10 @@
+const { getAllLaunches } = require('../../models/planets.model');
+
+
+function httpGetAllPlanets (req, res) {
+    return res.status(200).json(getAllLaunches());
+}
+
+module.exports = { 
+    httpGetAllPlanets,
+ };
